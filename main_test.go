1863590e5612ae4a1b7fb64ad789bf7a7d5349b9
cooler-SAI <cooler19860212@yahoo.com>
2024-11-07 21:18:53 +0300

    added base unit tests

---
+++ main_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestHTTPServer(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatalf("НCan not create request: %v", err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintln(w, "Welcome to the automated guessing game!")
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	expected := "Welcome to the automated guessing game!\n"
+	if rr.Body.String() != expected {
+		t.Errorf("Wrong answer: got %v, guess %v", rr.Body.String(), expected)
+	}
+}
+
+func TestRandomNumberGeneration(t *testing.T) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 100; i++ {
+		num := r.Intn(6)
+		if num < 0 || num > 5 {
+			t.Errorf("Number not valid")
+		}
+	}
+}
